fe06e867e6cb6ff3ed5fad70d4d890acfba78a46
aliyeganehfard <76558514+aliyeganehfard@users.noreply.github.com>
2022-01-16 21:03:54 +0330

    add postgres connection

---
+++ src/main/java/Model/PostgresConnection.java
@@ -0,0 +1,19 @@
+package Model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class PostgresConnection {
+    public static Connection connection;
+
+    static {
+        try {
+            Class.forName("org.postgresql.Driver");
+            connection = DriverManager.getConnection(
+                    "jdbc:postgresql://localhost:5432/postgres",
+                    "postgres","ali.1381");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
