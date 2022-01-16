31ffec6b19654823ae67a84406022be4c664d0ae
aliyeganehfard <76558514+aliyeganehfard@users.noreply.github.com>
2022-01-16 18:00:39 +0330

    add account class

---
+++ src/main/java/Model/Account.java
@@ -0,0 +1,54 @@
+package Model;
+
+public class Account {
+    private Integer id;
+    private Customer customer;
+    private Long balance;
+
+    public Account() {
+    }
+
+    public Account(Customer customer, Long balance) {
+        this.customer = customer;
+        this.balance = balance;
+    }
+
+    public Account(Integer id, Customer customer, Long balance) {
+        this.id = id;
+        this.customer = customer;
+        this.balance = balance;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Long getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Long balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id=" + id +
+                ", customer=" + customer +
+                ", balance=" + balance +
+                '}';
+    }
+}
