326abcc46de8bce12d12a3be2b5efbbce436bae3
aliyeganehfard <76558514+aliyeganehfard@users.noreply.github.com>
2022-01-17 17:40:30 +0330

    add bank employee repository

---
+++ src/main/java/Model/BankEmployeeRepository.java
@@ -0,0 +1,112 @@
+package Model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.List;
+
+public class BankEmployeeRepository {
+    private Connection connection = PostgresConnection.connection;
+    private String query;
+
+    public BankEmployeeRepository() {
+    }
+
+    public Integer insert(BankEmployee bankEmployee) {
+        Integer id = null;
+        try {
+            query = "insert into BSM_bank_employee (bank_branch_id, employee_name, employee_national_code) " +
+                    "VALUES (?,?,?);";
+            PreparedStatement preparedStatement =
+                    connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            preparedStatement.setInt(1, bankEmployee.getBankBranch().getId());
+            preparedStatement.setString(2, bankEmployee.getName());
+            preparedStatement.setString(3, bankEmployee.getNationalCode());
+            preparedStatement.executeUpdate();
+            ResultSet resultSet = preparedStatement.getGeneratedKeys();
+            if (resultSet.next()) {
+                id = resultSet.getInt(1);
+            }
+            preparedStatement.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return id;
+    }
+
+    public void delete(BankEmployee bankEmployee) {
+        try {
+            query = "delete from BSM_bank_employee " +
+                    "where id = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, bankEmployee.getId());
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public BankEmployee findById(Integer id) {
+        BankEmployee bankEmployee = null;
+        try {
+            query = "select * from BSM_bank_employee BE " +
+                    "inner join BSM_bank_branch Bbb on Bbb.id = BE.bank_branch_id " +
+                    "inner join BSM_bank Bb on Bb.id = Bbb.bank_id " +
+                    "where id =?";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                bankEmployee = new BankEmployee(
+                        resultSet.getInt("id"),
+                        new BankBranch(resultSet.getInt("bank_branch_id"),
+                                new Bank(resultSet.getInt("bank_id"), resultSet.getString("bank_name")),
+                                resultSet.getString("manager_name"),
+                                resultSet.getString("manager_national_code"),
+                                resultSet.getString("bank_address")
+                        ),
+                        resultSet.getString("employee_name"),
+                        resultSet.getString("employee_national_code"),
+                        resultSet.getString("employee_phone")
+                );
+            }
+            preparedStatement.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return bankEmployee;
+    }
+
+    //    show all bank branch employee
+    public List<BankEmployee> findAll(BankEmployee bankEmployee) {
+        List<BankEmployee> bankEmployees = null;
+        try {
+            query = "select * " +
+                    "from BSM_bank_employee " +
+                    "where bank_branch_id =?";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, bankEmployee.getBankBranch().getId());
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                bankEmployees.add(new BankEmployee(
+                                resultSet.getInt("id"),
+                                new BankBranch(resultSet.getInt("bank_branch_id"),
+                                        new Bank(resultSet.getInt("bank_id"), resultSet.getString("bank_name")),
+                                        resultSet.getString("manager_name"),
+                                        resultSet.getString("manager_national_code"),
+                                        resultSet.getString("bank_address")
+                                ),
+                                resultSet.getString("employee_name"),
+                                resultSet.getString("employee_national_code"),
+                                resultSet.getString("employee_phone")
+                        )
+                );
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return bankEmployees;
+    }
+}
