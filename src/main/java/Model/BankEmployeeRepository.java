40d9cc6c2ba9e5ecc164d715535e0b471eaa4926
aliyeganehfard <76558514+aliyeganehfard@users.noreply.github.com>
2022-01-19 23:33:43 +0330

    fixed add bank employee repository problem

--- src/main/java/Model/BankEmployeeRepository.java
+++ src/main/java/Model/BankEmployeeRepository.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class BankEmployeeRepository {
@@ -11,18 +12,33 @@ public class BankEmployeeRepository {
     private String query;
 
     public BankEmployeeRepository() {
+        try {
+            query="create table if not exists BSM_bank_employee " +
+                    "( " +
+                    "    id                     serial primary key, " +
+                    "    bank_branch_id         Integer, " +
+                    "    employee_name          varchar(100), " +
+                    "    employee_national_code char(10), " +
+                    "    employee_phone         char(11), " +
+                    "    constraint BSM_BB_ID foreign key (bank_branch_id) references BSM_bank_branch (id) " +
+                    ")";
+            connection.prepareStatement(query).execute();
+        }catch (Exception e){
+            System.out.println(e);
+        }
     }
 
     public Integer insert(BankEmployee bankEmployee) {
         Integer id = null;
         try {
-            query = "insert into BSM_bank_employee (bank_branch_id, employee_name, employee_national_code) " +
-                    "VALUES (?,?,?);";
+            query = "insert into BSM_bank_employee (bank_branch_id, employee_name, employee_national_code,employee_phone) " +
+                    "VALUES (?,?,?,?);";
             PreparedStatement preparedStatement =
                     connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
             preparedStatement.setInt(1, bankEmployee.getBankBranch().getId());
             preparedStatement.setString(2, bankEmployee.getName());
             preparedStatement.setString(3, bankEmployee.getNationalCode());
+            preparedStatement.setString(4, bankEmployee.getPhone());
             preparedStatement.executeUpdate();
             ResultSet resultSet = preparedStatement.getGeneratedKeys();
             if (resultSet.next()) {
@@ -80,14 +96,15 @@ public class BankEmployeeRepository {
     }
 
     //    show all bank branch employee
-    public List<BankEmployee> findAll(BankEmployee bankEmployee) {
-        List<BankEmployee> bankEmployees = null;
+    public List<BankEmployee> findAll(BankBranch bankBranch) {
+        List<BankEmployee> bankEmployees = new ArrayList<BankEmployee>();
         try {
-            query = "select * " +
-                    "from BSM_bank_employee " +
-                    "where bank_branch_id =?";
+            query = "select * from BSM_bank_employee " +
+                    "inner join BSM_bank_branch Bbb on Bbb.id = BSM_bank_employee.bank_branch_id " +
+                    "inner join BSM_bank Bb on Bb.id = Bbb.bank_id " +
+                    "where bank_branch_id=? ";
             PreparedStatement preparedStatement = connection.prepareStatement(query);
-            preparedStatement.setInt(1, bankEmployee.getBankBranch().getId());
+            preparedStatement.setInt(1, bankBranch.getId());
             ResultSet resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
                 bankEmployees.add(new BankEmployee(
