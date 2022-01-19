24975ef02ddd482a78fa93e31036816da3ac0264
aliyeganehfard <76558514+aliyeganehfard@users.noreply.github.com>
2022-01-19 23:32:37 +0330

    fixed add bank repository problem

--- src/main/java/Model/BankRepository.java
+++ src/main/java/Model/BankRepository.java
@@ -4,12 +4,24 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BankRepository {
     private Connection connection = PostgresConnection.connection;
     private String query;
 
     public BankRepository() {
+        try {
+            query="create table if not exists BSM_bank " +
+                    "( " +
+                    "    id serial primary key, " +
+                    "    bank_name varchar(100) " +
+                    ")";
+            connection.prepareStatement(query).execute();
+        }catch (Exception e){
+            System.out.println(e);
+        }
     }
 
     public Integer insert(Bank bank) {
@@ -31,15 +43,36 @@ public class BankRepository {
         return id;
     }
 
+    public List<Bank> findAll() {
+        List<Bank> banks = new ArrayList<Bank>();
+        try {
+            query = "select * from BSM_bank " +
+                    "where id=?";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                banks.add(new Bank(
+                                resultSet.getInt("id"),
+                                resultSet.getString("bank_name")
+                        )
+                );
+            }
+            preparedStatement.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return banks;
+    }
+
     public Bank findById(Integer id) {
         Bank bank = null;
         try {
             query = "select * from BSM_bank " +
                     "where id=?";
             PreparedStatement preparedStatement = connection.prepareStatement(query);
-            preparedStatement.setInt(1,id);
+            preparedStatement.setInt(1, id);
             ResultSet resultSet = preparedStatement.executeQuery();
-            if (resultSet.next()){
+            if (resultSet.next()) {
                 bank = new Bank(
                         resultSet.getInt("id"),
                         resultSet.getString("bank_name")
@@ -52,6 +85,33 @@ public class BankRepository {
         return bank;
     }
 
+    //    show all branch of bank
+    public List<BankBranch> findAllBranch(Bank bank) {
+        List<BankBranch> bankBranches = new ArrayList<BankBranch>();
+        try {
+            query = "select * from BSM_bank " +
+                    "inner join BSM_bank_branch Bbb on BSM_bank.id = Bbb.bank_id " +
+                    "where BSM_bank.id =?";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, bank.getId());
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                bankBranches.add(
+                        new BankBranch(
+                                resultSet.getInt("id"),
+                                new Bank(resultSet.getInt("bank_id"), resultSet.getString("bank_name")),
+                                resultSet.getString("manager_name"),
+                                resultSet.getString("manager_national_code"),
+                                resultSet.getString("bank_address")
+                        )
+                );
 
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+            System.out.println("hi");
+        }
+        return bankBranches;
+    }
 
 }
