989324d3c9fbda85a4ce4df7ab14639a26f4d6da
aliyeganehfard <76558514+aliyeganehfard@users.noreply.github.com>
2022-01-19 23:38:20 +0330

    add bank branch service

---
+++ src/main/java/Controller/BankBranchService.java
@@ -0,0 +1,48 @@
+package Controller;
+
+import Model.*;
+
+import java.util.List;
+
+public class BankBranchService {
+    private BankBranchRepository bankBranchRepository = new BankBranchRepository();
+    private BankEmployeeRepository bankEmployeeRepository = new BankEmployeeRepository();
+    private CustomerRepository customerRepository = new CustomerRepository();
+    private AccountRepository accountRepository = new AccountRepository();
+    private TransactionRepository transactionRepository = new TransactionRepository();
+
+    //    insert branch
+    public void insert(BankBranch bankBranch) {
+        bankBranchRepository.insert(bankBranch);
+    }
+
+    //    show all the bank branch
+    public List<BankBranch> findAll() {
+        return bankBranchRepository.findAll();
+    }
+
+    //    show selected bank branch
+    public BankBranch findById(Integer id) {
+        return bankBranchRepository.findById(id);
+    }
+
+    //    show all bank branch employee
+    public List<BankEmployee> findAllEmployee(BankBranch bankBranch) {
+        return bankEmployeeRepository.findAll(bankBranch);
+    }
+
+    //    show all customer of bank branch
+    public List<Customer> findAllCustomer(BankBranch bankBranch) {
+        return customerRepository.findAll(bankBranch);
+    }
+
+//    show all account of bank branch
+    public List<Account> findAllAccount(BankBranch bankBranch){
+        return accountRepository.findAll(bankBranch);
+    }
+
+//    show all transaction of bank branch
+//    public List<Transaction> findAllTransaction(BankBranch bankBranch){
+//        return transactionRepository.findById();
+//    }
+}
